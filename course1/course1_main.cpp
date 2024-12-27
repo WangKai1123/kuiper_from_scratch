4e50e908cfc82e5b177d1666d3a6ac71415879ca
WangKai1123 <wk563006423@163.com>
2024-12-27 22:46:36 +0800

    init

---
+++ course1/course1_main.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <glog/logging.h>
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    google::InitGoogleLogging("Kuiper");
+
+
+}
