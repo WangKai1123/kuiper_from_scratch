29600f0cc96c1d483fe72a392428311ad753471d
WangKai1123 <wk563006423@163.com>
2025-01-12 16:53:00 +0800

    new feature:增加course3

---
+++ course3/build/CMakeFiles/kuiper_datawhale_course3.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wk/kuiper_from_scratch/course3/main.cpp" "CMakeFiles/kuiper_datawhale_course3.dir/main.cpp.o" "gcc" "CMakeFiles/kuiper_datawhale_course3.dir/main.cpp.o.d"
+  "/home/wk/kuiper_from_scratch/course3/source/ir.cpp" "CMakeFiles/kuiper_datawhale_course3.dir/source/ir.cpp.o" "gcc" "CMakeFiles/kuiper_datawhale_course3.dir/source/ir.cpp.o.d"
+  "/home/wk/kuiper_from_scratch/course3/source/load_data.cpp" "CMakeFiles/kuiper_datawhale_course3.dir/source/load_data.cpp.o" "gcc" "CMakeFiles/kuiper_datawhale_course3.dir/source/load_data.cpp.o.d"
+  "/home/wk/kuiper_from_scratch/course3/source/runtime_attr.cpp" "CMakeFiles/kuiper_datawhale_course3.dir/source/runtime_attr.cpp.o" "gcc" "CMakeFiles/kuiper_datawhale_course3.dir/source/runtime_attr.cpp.o.d"
+  "/home/wk/kuiper_from_scratch/course3/source/runtime_ir.cpp" "CMakeFiles/kuiper_datawhale_course3.dir/source/runtime_ir.cpp.o" "gcc" "CMakeFiles/kuiper_datawhale_course3.dir/source/runtime_ir.cpp.o.d"
+  "/home/wk/kuiper_from_scratch/course3/source/runtime_op.cpp" "CMakeFiles/kuiper_datawhale_course3.dir/source/runtime_op.cpp.o" "gcc" "CMakeFiles/kuiper_datawhale_course3.dir/source/runtime_op.cpp.o.d"
+  "/home/wk/kuiper_from_scratch/course3/source/store_zip.cpp" "CMakeFiles/kuiper_datawhale_course3.dir/source/store_zip.cpp.o" "gcc" "CMakeFiles/kuiper_datawhale_course3.dir/source/store_zip.cpp.o.d"
+  "/home/wk/kuiper_from_scratch/course3/source/tensor.cpp" "CMakeFiles/kuiper_datawhale_course3.dir/source/tensor.cpp.o" "gcc" "CMakeFiles/kuiper_datawhale_course3.dir/source/tensor.cpp.o.d"
+  "/home/wk/kuiper_from_scratch/course3/test/test.cpp" "CMakeFiles/kuiper_datawhale_course3.dir/test/test.cpp.o" "gcc" "CMakeFiles/kuiper_datawhale_course3.dir/test/test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
